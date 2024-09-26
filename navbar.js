79f080e015be2d16a695a3572fc0127b0040bd7d
kezia03 <kezianatalli@gmail.com>
2024-09-26 08:38:18 +0700

    comment

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) { 
+window.addEventListener('scroll', function() { /* Ketika user scoll, function akan berjalan */
+    const navbar = document.querySelector('.navbar'); /* Ambil class 'navbar' dri index dan menyimpannya ke variabel navbar */
+    if (window.scrollY > 50) { /* Jika halaman yg di scroll > 50px, kondisi = true */
         navbar.classList.add('scrolled');
     } else {
         navbar.classList.remove('scrolled');
